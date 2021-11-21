843eda6f029a3eaba55901ac76f04f30078b0d43
mehrabix <mehrabi@post.com>
2021-11-21 21:15:07 +0330

    header structure

--- components/header.jsx
+++ components/header.jsx
@@ -20,7 +20,7 @@ const header = () => {
                             <span class="font-IranSansMedium text-xl tracking-tight">فاطمه آقاجانی</span>
                         </div>
                         <div class="block lg:hidden">
-                            <button onClick={() => handleOpenCloseMenu()} class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                            <button onClick={() => handleOpenCloseMenu()} class="flex items-center px-3 py-2 border rounded text-teal-200 ml-3 border-teal-400 hover:text-white hover:border-white">
                                 <svg class="fill-current text-white h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
                             </button>
                         </div>
